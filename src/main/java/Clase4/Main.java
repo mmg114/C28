e97d51d69bc1edf2e3676bac7dc87dcddaf8f8cd
Mauricio Munoz Guzman <mauricio.m@moabits.com>
2024-09-17 19:48:47 -0500

    array

--- src/main/java/Clase4/Main.java
+++ src/main/java/Clase4/Main.java
@@ -1,5 +1,10 @@
 package Clase4;
 
+import Clase3.Moto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -16,5 +21,80 @@ public class Main {
             System.out.println("Es menor de edad");
         }
          var esVerdad= edad >=18 ?true:false;
+
+        int day = 3;
+
+        String dayName1 = switch (day) {
+            case 1 -> "Lunes";
+            case 2 -> "Martes";
+            case 3 -> "Miércoles";
+            case 4 -> "Jueves";
+            case 5 -> "Viernes";
+            case 6 -> "Sábado";
+            case 7 -> "Domingo";
+            default -> "Día inválido";
+        };
+
+        System.out.println("El día es: " + dayName1);
+
+    String dayName;
+
+    // Usando switch para determinar el día de la semana
+        switch (day) {
+        case 1:
+            dayName = "Lunes";
+            break;
+        case 2:
+            dayName = "Martes";
+            break;
+        case 3:
+            dayName = "Miércoles";
+            break;
+        case 4:
+            dayName = "Jueves";
+            break;
+        case 5:
+            dayName = "Viernes";
+            break;
+        case 6:
+            dayName = "Sábado";
+            break;
+        case 7:
+            dayName = "Domingo";
+            break;
+        default:
+            dayName = "Día inválido";
+            break;
+    }
+
+        System.out.println("El día es: " + dayName);
+
+
+
+        int[] arreglo = new int[5];
+        String[] arregloPalabras = new String[5];
+        String[] arregloPalabras2 = new String[5];
+        Moto[] misMotos= new Moto[200];
+
+
+        Long[] miArregloLong;
+
+
+
+
+
+
+
+
+        miArregloLong= new Long[10];
+
+        List<String> miLista = new ArrayList<>();
+        miLista.add("Hola");
+        miLista.add("Hola");
+        miLista.add("Hola");
+        miLista.add("Hola");
+        miLista.add("Hola");
+
+
     }
 }
