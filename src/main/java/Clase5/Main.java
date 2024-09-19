91d052c4d0ceb15db9c7e9724ffbb4ab91fa75be
Mauricio Munoz Guzman <mauricio.m@moabits.com>
2024-09-18 19:24:33 -0500

    List

---
+++ src/main/java/Clase5/Main.java
@@ -0,0 +1,25 @@
+package Clase5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] arregloPalabras = new String[5];
+
+        arregloPalabras[0]= "Mauricio";
+        arregloPalabras[2]= "Sara";
+
+        String[] materias = {"Matematicas","programacion","Calculo"};
+        System.out.println(materias[0]);
+        System.out.println("El tamaño de mi arreglo es : "+arregloPalabras.length);
+
+        List<String> myList= List.of("Perros calientes", "Arepa", "Carne asada");
+        List<String> myList2= new ArrayList<>();
+
+        myList2.add("Mango");
+        myList2.add("Pera");
+       // myList.add("Gaseosa");
+
+    }
+}
